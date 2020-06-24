70816bb54894fadc9f3997868eed2fd021f44111
Glitch (hello-tensorflow) <none>
2020-06-24 04:24:23 +0000

    🏏🌏 Checkpoint ./vrscript.js:276325/1070

--- vrscript.js
+++ vrscript.js
@@ -1,3 +1,17 @@
+var fullColorHex = function(r,g,b) {   
+  var rgbToHex = function (rgb) { 
+    var hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+         hex = "0" + hex;
+    }
+    return hex;
+  };
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return '#'+red+green+blue;
+};
+
 let graphSize = 10
 let graph = document.createElement('a-entity')
 //z-axis
@@ -5,9 +19,10 @@ for(let z=-graphSize;z<graphSize;z++){
   //x-axis
   for(let x=-graphSize;x<graphSize;x++){
     let point = document.createElement('a-sphere')
+    let normalizedHeight = parseInt(Math.sin(x*z)*255/2+128)
     point.setAttribute('scale', '0.1 0.1 0.1')
-    point.setAttribute('color', 'rgb(0,0,255)')
-    point.setAttribute('position', {x: x, y: Math.sin(x), z: z})
+    point.setAttribute('color', fullColorHex(normalizedHeight,0,111))
+    point.setAttribute('position', {x: x, y: Math.sin(x*z), z: z})
     graph.appendChild(point)
   }
 }
