526c974c65f4e4e2283696bd42a22b907d9a1d2c
Glitch (hello-tensorflow) <none>
2020-06-24 04:13:22 +0000

    📯🤗 Checkpoint ./index.html:276325/351 ./vrscript.js:276325/669

--- vrscript.js
+++ vrscript.js
@@ -1,6 +1,15 @@
-for(let i=-10;i<10;i++){
-  let point = document.createElement('a-sphere')
-  point.setAttribute('color','blue')
-  point.setAttribute('position', {x: 10, y: i*i*i, z: i*i})
-  document.querySelector('a-scene').appendChild(point)
+let graphSize = 10
+let graph = document.createElement('a-entity')
+//z-axis
+for(let z=-graphSize;z<graphSize;z++){
+  //x-axis
+  for(let x=-graphSize;x<graphSize;x++){
+    let point = document.createElement('a-sphere')
+    point.setAttribute('scale', '0.1 0.1 0.1')
+    point.setAttribute('color', 'rgb(0,0,255)')
+    point.setAttribute('position', {x: x, y: Math.sin(x), z: z})
+    graph.appendChild(point)
+  }
 }
+graph.setAttribute('position', {x: 0, y: 0, z: -10})
+document.querySelector('a-scene').appendChild(graph)
