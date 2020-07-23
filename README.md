f97a61ef5337daf49a95c1f8f18f2ab17c4b7b86
Glitch (hello-tensorflow) <none>
2020-07-23 02:10:13 +0000

    👩‍❤️‍💋‍👩🚴 Checkpoint ./index.html:276325/118 ./README.md:276325/681

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
-hello tensorflow
+A-frame with Tensorflow.js
 =================
 
-This is the [fit a curve to a bunch of data](https://js.tensorflow.org/tutorials/fit-curve.html) example from [TensorFlow](https://js.tensorflow.org/), but with different words, a looooooot 
-more code comments, and prettier graphs. 
-
-The [demo](https://hello-tensorflow.glitch.me/) has a lot of words about what Machine Learning is and how the algorithm works overall.
-
-I commented basically every line of the code with what the algorithm or TensorFlow is doing (especially when
-      TensorFlow is actually doing a looooot of heavy lifting behind the scenes). I hope it helps!
-      
-💖
+Backend inspired by 💖
--- script.js
+++ script.js
@@ -1,134 +1,41 @@
-/*
-Here's the TL; DR of this code example:
-- Someone gives us some data that was generated according to a cubic formula (i.e. something of
-the form y = a * x ^ 3 + b * x^2 + c * x + d, for a bunch of xs), but doesn't
-tell us the values of these a,b,c,d coefficients
-- Just by looking at these (x,y) points we were given, we'd like to figure out the coefficients
-they were generated from.
+var fullColorHex = function(r,g,b) {
+  let lower = 0
+  let upper = 255
+  r = clamp(r, lower, upper)
+  g = clamp(g, lower, upper)
+  b = clamp(b, lower, upper)
+  var rgbToHex = function (rgb) { 
+    var hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+         hex = "0" + hex;
+    }
+    return hex;
+  };
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return '#'+red+green+blue;
+};
 
-And we do this here!
-
-Look out for comments that have 👉 in the code: it means that's code 
-you can play around with!
-*/
-
-// Sup globals. Fight me. Plots are hard.
-const Data = {
-  training: {x:[], y:[]},   // the initial data set we're given
-  prediction: {x:[], y:[]}, // what we're predicting based on the coefficients
-  learning: {x:[], y:[]}    // what we're predicting while learning.
+function clamp(val, min, max) {
+    return parseInt(val > max ? max : val < min ? min : val);
 }
 
-let NUM_POINTS;             // how many data points in the set.
-
-// These are the coefficients we're trying to learn. 
-// They're all TensorFlows scalars (fancy for "number") and
-// initialized to random numbers.
-const a = tf.variable(tf.scalar(Math.random()));
-const b = tf.variable(tf.scalar(Math.random()));
-const c = tf.variable(tf.scalar(Math.random()));
-const d = tf.variable(tf.scalar(Math.random()));
-
-init();
-
-function init() {
-  NUM_POINTS = parseInt(document.getElementById('points').value || 100);
-  const defaultCoeffs = {
-    a: parseFloat(document.getElementById('i_a').value || -0.8),
-    b: parseFloat(document.getElementById('i_b').value || -0.2),
-    c: parseFloat(document.getElementById('i_c').value || 0.9),
-    d: parseFloat(document.getElementById('i_d').value || 0.5)
-  }
-  
-  // Fake the training data. 
-  // 👉 you can play with these numbers if you want to generate a 
-  // different initial data set!
-  Data.training = generateData(NUM_POINTS, defaultCoeffs);
-  
-  // Firt, see what our predictions would look like with random coefficients
-  const coeff = {
-    a: a.dataSync()[0],
-    b: b.dataSync()[0],
-    c: c.dataSync()[0],
-    d: d.dataSync()[0],
-  };
-  
-  Data.prediction = generateData(NUM_POINTS, coeff);
-  plot();
-}
-
-/*
- * Plots a pretty graph with all the data we have!
- * Uses plotly.js
- */
-
-function plot() {
-  const trace1 = {
-    x: Data.training.x,
-    y: Data.training.y,
-    mode: 'lines+markers',
-    name: 'Training',
-    marker: { size: 12, color:'#29B6F6' }
-  };
-
-  const trace2 = {
-    x: Data.prediction.x,
-    y: Data.prediction.y,
-    mode: 'lines+markers',
-    name: 'Initial Prediction',
-    marker: { size: 12, color: '#F06292' }
-  };
-  
-  let trace3 = {};
-  if (Data.learning) {
-    trace3 = {
-      x: Data.learning.x,
-      y: Data.learning.y,
-      mode: 'lines+markers',
-      name: 'Learning',
-      marker: { size: 12, color: '#00E676' }
-    };
-  }
-  
-  const layout = {
-    margin: {
-      l: 20, r: 0, b: 0, t: 0, 
-      pad:0
-    },
-    legend: {
-				xanchor:"left",
-				yanchor:"top",
-				y: 1,  //tbh i don't know what these numbers mean?!
-        x: 1,
-				orientation: "v"
-	  },
-  };
-  Plotly.newPlot('graph', [trace1, trace2, trace3], layout, {displayModeBar: true});
-}  
-
-/*
- * Generates data according to the formula:
- * y = a * x ^ 3 + b * x^2 + c * x + d
- */
-function generateData(points, {a, b, c, d}) {
-  let x = [];
-  let y = [];
-  
-  // Normalize the x values to be between -1 and 1.
-  // 🚨 This is super important! TensorFlow requires this
-  // for the algorithm to work, and if you don't do this
-  // you learn NaN for every coefficient.
-  const xs = tf.randomUniform([points], -1, 1);  // magic TF to give you points between [-1, 1]
-  for (let i = 0; i < points; i++) {
+var generateData = function(graphSize,{a,b,c,d}) {
+  let x = []
+  let z = []
+  let y = []
+  const xs = tf.randomUniform([graphSize], -1, 1);
+  const zs = tf.randomUniform([graphSize], -1, 1);
+  for (let i = 0; i < graphSize; i++) {
     x[i] = xs.get(i);  // goes from a TF tensor (i.e. array) to a number.
+    z[i] = zs.get(i);
   }
-  x = x.sort(function(a,b){return a - b})
+  //z-axis
+  for(let i=0;i<x.length;i++){
+    y[i] = (a * x[i]*x[i] + b * x[i] + c * z[i]*z[i] + d * z[i])
+  }
   
-  // Generate the random y values.  
-  for (let i = 0; i < points; i++) {
-    const val = x[i];
-    y[i] = a * (val*val*val) + b * (val*val) + c * val + d;
-  }
   
   // Normalize the y values to be between 0 and 1
   const ymin = Math.min(...y);
@@ -140,13 +47,66 @@ function generateData(points, {a, b, c, d}) {
     y[i] = (y[i] - ymin) / yrange;
   }
   
-  return {x:x, y:y}
+  return {x:x, z:z, y:y}
 }
 
-/*
- * Learn the coefficients.
- * Very much based on https://github.com/tensorflow/tfjs-examples/blob/master/polynomial-regression-core/index.js
- */
+const Data = {
+  training: {x:[], z:[], y:[]},   // the initial data set we're given
+  prediction: {x:[], z:[], y:[]}, // what we're predicting based on the coefficients
+  learning: {x:[], z:[], y:[]}    // what we're predicting while learning.
+}
+
+const a = tf.variable(tf.scalar(Math.random()));
+const b = tf.variable(tf.scalar(Math.random()));
+const c = tf.variable(tf.scalar(Math.random()));
+const d = tf.variable(tf.scalar(Math.random()));
+
+function init(){
+  NUM_POINTS = parseInt(document.getElementById('points').value || 100);
+  const defaultCoeffs = {
+    a: parseFloat(document.getElementById('i_a').value || -0.8),
+    b: parseFloat(document.getElementById('i_b').value || -0.2),
+    c: parseFloat(document.getElementById('i_c').value || 0.9),
+    d: parseFloat(document.getElementById('i_d').value || 0.5)
+  }
+  Data.training = generateData(NUM_POINTS, defaultCoeffs);
+  const coeff = {
+    a: a.dataSync()[0],
+    b: b.dataSync()[0],
+    c: c.dataSync()[0],
+    d: d.dataSync()[0],
+  };
+  Data.prediction = generateData(NUM_POINTS, coeff);
+  createGraph(Data.training, "train")
+  createGraph(Data.prediction, "predict")
+}
+
+
+function createGraph(data,set){
+  let oldgraph = document.querySelector('.graph'+set)
+  document.querySelector('a-scene').removeChild(oldgraph)
+  let graph = document.createElement('a-entity')
+  for(let i=0;i<data.x.length;i++){
+    let point = document.createElement('a-sphere')
+    point.setAttribute('scale', '0.1 0.1 0.1')
+    if(set=="train"){
+      point.setAttribute('color', fullColorHex(data.y[i]*128+128,-data.y[i]*33+66,0))
+    } else if(set=="predict"){
+      point.setAttribute('color', fullColorHex(data.y[i]*128+128,-data.y[i]*33+66,0))
+      point.setAttribute('blending', 'subtractive')
+      point.setAttribute('opacity', '0.3')
+    }
+    point.setAttribute('position', {x: data.x[i], y: data.y[i], z: data.z[i]})
+    graph.appendChild(point)
+  }
+  graph.setAttribute('position', {x: 0, y: 0, z: 0})
+  graph.setAttribute('rotation', {x: 0, y: 90, z: 0})
+  document.querySelector('a-scene').appendChild(graph)
+  graph.classList.add('graph'+set)
+}
+
+init()
+
 async function doALearning() {
   document.querySelector('#iteration_counter').disabled = true;
   // Create an optimizer. This is the thing that does the learning.
@@ -204,7 +164,7 @@ async function doALearning() {
   const optimizer = getOptimizer()
     
   // Use the training data, and do numIteration passes over it. 
-  await train(tf.tensor1d(Data.training.x), tf.tensor1d(Data.training.y), numIterations);
+  await train(tf.tensor1d(Data.training.x), tf.tensor1d(Data.training.z), tf.tensor1d(Data.training.y), numIterations);
   
   // Once that is done, this has updated our coefficients! 
   // Here you could see what our predictions look like now, and use them!
@@ -221,7 +181,8 @@ async function doALearning() {
   /*
    * This does the training of the model.
    */
-  async function train(xs, ys, numIterations) {
+  async function train(xs, zs, ys, numIterations) {
+    console.log(xs.print(),zs.print())
     for (let iter = 0; iter < numIterations; iter++) {
       // Plot where we are at this step.
       const coeff = {
@@ -230,18 +191,23 @@ async function doALearning() {
         c: c.dataSync()[0],
         d: d.dataSync()[0],
       };
-      Data.learning = generateData(NUM_POINTS, coeff);
-      plot();
+      Data.prediction = generateData(NUM_POINTS, coeff);
+      createGraph(Data.prediction, "predict")
+
   
       // Learn! This is where the step happens, and when the training takes place.
       optimizer.minimize(() => {
         // Using our estimated coeff, predict all the ys for all the xs 
-        const pred = predict(xs);
+        const pred = predict(xs, zs);
         
         // Need to return the loss i.e how bad is our prediction from the 
         // correct answer. The optimizer will then adjust the coefficients
         // to minimize this loss.
-        return loss(pred, ys);
+        let loss_value = loss(pred, ys)
+        if(loss_value.dataSync()[0]<0.001){
+          iter=numIterations
+        }
+        return loss_value;
       });
       document.querySelector('#iteration_counter').innerHTML = iter+1
       // Use tf.nextFrame to not block the browser.
@@ -252,17 +218,17 @@ async function doALearning() {
   }
   
   /*
-   * Predicts all the y values for all the x values.
+   * Predicts all the y values for all the x and z values.
    */
-  function predict(x) {
+  function predict(x,z) {
     // Calculate a y according to the formula
     // y = a * x ^ 3 + b * x ^ 2 + c * x + d
     // where a, b, c, d are the coefficients we have currently calculated.
     return tf.tidy(() => {
-      return a.mul(x.pow(tf.scalar(3, 'int32')))
-        .add(b.mul(x.square()))
-        .add(c.mul(x))
-        .add(d);
+      return a.mul(x.square())
+        .add(b.mul(x))
+        .add(c.mul(z.square()))
+        .add(d.mul(z))
     });
   }
   
@@ -279,6 +245,8 @@ async function doALearning() {
     
     // Also, this is also why TensorFlow is great! Doing this by hand sucks!
     const error = prediction.sub(labels).square().mean();
+    // let errval = Number.parseFloat(error.dataSync()[0]*5).toPrecision(4)
+    // document.querySelector('#loss').setAttribute('scale',  {x:1,y:errval,z:1})
     return error;
   }
 }
